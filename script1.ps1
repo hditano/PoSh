16e55a02f21f3a649d8ef0f2a938a85cd12e0654
Hernan Di Tano <hditano@gmail.com>
2023-07-01 21:05:01 +0200

    added update

--- script1.ps1
+++ script1.ps1
@@ -5,31 +5,49 @@ foreach ($item in $items)
     if($item -eq 10) { Write-Output $item }
 }
 
-function Get-Array {
-    param (
-        [string[]]$ParameterName
-    )
 
-    foreach($Parameter in $ParameterName){
-        $Results = Get-DataReturn -ParameterName $Parameter
-    }
+function Get-Array {
+
+    $Results = Get-DataReturn -ParameterName $Parameter -ParameterNumber $Number
 
     [PSCustomObject]@{
-        Name = $Parameter
-        Age = $Results.Count
+        Name = $Results.String
+        Age = $Results.Number
     }
 }
 
 function Get-DataReturn {
-    param(
-        [string[]]$ParameterName
-    )
+    $Data = "Data for Parameter"
+    $Number = 23
 
-    foreach($Parameter in $ParameterName){
-        $Data = "Data for Parameter"
+    $MyObject = [PSCustomObject]@{
+        String = $Data
+        Number = $Number
     }
 
-    $Data
+   $MyObject 
 }
 
-Get-Array -ParameterName "Hello", "23"
+function Get-Api {
+    $Result = Invoke-RestMethod -Uri "https://api.coinpaprika.com/v1/coins/btc-bitcoin"
+
+    foreach($item in $Result){
+        $MyObject = [PSCustomObject]@{
+            Name =  $item.Name
+            Hash = $item.open_source
+        }
+    }
+
+    $Result | Format-Table -Property name, hash_algorithm
+}
+
+function Get-Input{
+  Write-Output "Hello from the Outside"
+  $Result = Read-Host -Prompt "Input your name"
+  Write-Output "Name is $Result"
+
+}
+
+Get-Api
+Get-Input
+
