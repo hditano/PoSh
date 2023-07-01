9857b8971365b1094a3c33c052d173c18e107ddb
Hernan Di Tano <hditano@gmail.com>
2023-07-01 05:56:18 +0200

    first commit

---
+++ script1.ps1
@@ -0,0 +1,35 @@
+﻿$items = 10, 20, 30, 40
+
+foreach ($item in $items)
+{
+    if($item -eq 10) { Write-Output $item }
+}
+
+function Get-Array {
+    param (
+        [string[]]$ParameterName
+    )
+
+    foreach($Parameter in $ParameterName){
+        $Results = Get-DataReturn -ParameterName $Parameter
+    }
+
+    [PSCustomObject]@{
+        Name = $Parameter
+        Age = $Results.Count
+    }
+}
+
+function Get-DataReturn {
+    param(
+        [string[]]$ParameterName
+    )
+
+    foreach($Parameter in $ParameterName){
+        $Data = "Data for Parameter"
+    }
+
+    $Data
+}
+
+Get-Array -ParameterName "Hello", "23"
